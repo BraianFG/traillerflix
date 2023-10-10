5b2d929ab4f570e58357e8ee76adbd0467bdd553
BraianFG <55467665+BraianFG@users.noreply.github.com>
2023-10-09 21:58:08 -0300

    Update tablas.sql
    Primero cargar tabla de base de datos antes de hacer la prueba

--- tablas.sql
+++ tablas.sql
@@ -1,20 +1,19 @@
--- Crear la tabla crea base de datos
+-- Crear la base de datos
 CREATE SCHEMA trailerflix;
 
 -- Referencia a la base de datos
 USE trailerflix;
 
--- Crear la tabla "categoría"
+-- Crear la tabla "categorias"
 CREATE TABLE categorias (
-    id INT,
-    categoria VARCHAR(254),
-    FOREIGN KEY(CategoriaID) REFERENCES categorias(categoria)
+    categoriaID INT PRIMARY KEY,
+    categoria VARCHAR(254)
 );
 
 -- Crear la tabla "generos"
 CREATE TABLE generos (
-    Generoid INT,
-    genero VARCHAR(254) 
+    generoID INT PRIMARY KEY,
+    genero VARCHAR(254)
 );
 
 -- Crear la tabla "actores"
@@ -25,30 +24,31 @@ CREATE TABLE actores (
 
 -- Crear la tabla "contenido"
 CREATE TABLE contenido (
-    id INT PRIMARY KEY NOT NULL ,
-    FOREIGN KEY (id) references reparto_genero(generoID),
-	FOREIGN KEY (id) references reparto_catalogo(reparto_catalogoID),
+    id INT PRIMARY KEY,
     titulo VARCHAR(254),
     poster VARCHAR(254),
-    categoriaID int,
-    FOREIGN KEY (id) REFERENCES reparto(categoria),
-    generoID int,
+    categoriaID INT,
+    generoID INT,
     resumen VARCHAR(254),
     temporadas INT,
-    repartoID int,
-    trailer VARCHAR(254)
+    repartoID INT,
+    trailer VARCHAR(254),
+    FOREIGN KEY (categoriaID) REFERENCES categorias(categoriaID),
+    FOREIGN KEY (generoID) REFERENCES generos(generoID),
+    FOREIGN KEY (repartoID) REFERENCES actores(id)
 );
 
 -- Crear la tabla "reparto_catalogo"
 CREATE TABLE reparto_catalogo (
-    FOREIGN KEY (reparto_catalogoID) REFERENCES contenido(id),
-    reparto_catalogoID INT,
-    catalogoID INT
+    reparto_catalogoID INT PRIMARY KEY,
+    catalogoID INT,
+    FOREIGN KEY (catalogoID) REFERENCES contenido(id)
 );
 
 -- Crear la tabla "reparto_genero"
 CREATE TABLE reparto_genero (
-  catalogoID INT,
-  generoID INT,
-  FOREIGN KEY (catalogoID) REFERENCES  generos(generoID)
+    catalogoID INT,
+    generoID INT,
+    FOREIGN KEY (catalogoID) REFERENCES contenido(id),
+    FOREIGN KEY (generoID) REFERENCES generos(generoID)
 );
